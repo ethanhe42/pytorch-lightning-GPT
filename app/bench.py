f67340efe33e85325d86d2059c7cdf17424a0a5b
Luca Antiga <luca@lightning.ai>
2022-12-20 16:53:14 +0100

    Add benchmarks

---
+++ app/bench.py
@@ -0,0 +1,85 @@
+#! ltt install --pytorch-channel nightly torch --upgrade 
+#! pip install git+https://github.com/Lightning-AI/lightning-minGPT
+#! curl https://cs.stanford.edu/people/karpathy/char-rnn/shakespeare_input.txt --create-dirs -o ${HOME}/data/input.txt -C -
+
+
+# pip install -U --pre torch --extra-index-url https://download.pytorch.org/whl/nightly/cu117
+
+import os
+
+import torch
+from torch.utils.data import DataLoader
+
+import lightning as L
+from lightning_mingpt import data, models, callbacks, bench
+
+
+class GPTBench(bench.Bench):
+    def __init__(self, num_runs, *args, **kwargs):
+        super().__init__(num_runs, *args, **kwargs)
+        self.num_workers = 4
+        self.batch_size = 64
+        self.max_epochs = 10
+        self.precision = 16
+        self.model_type = "gpt2"
+
+    def create(self):
+        torch.set_float32_matmul_precision("high")
+
+        with open(os.path.expanduser("~/data/input.txt")) as f:
+            text = f.read()
+
+        dataset = data.CharDataset(text, block_size=128)
+        dataloader = DataLoader(
+            dataset,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers
+        )
+
+        model = models.GPT(
+            vocab_size=dataset.vocab_size,
+            block_size=dataset.block_size,
+            model_type=self.model_type,
+        )
+
+        return model, dataloader
+
+    def train(self, model, dataloader):
+        trainer = L.Trainer(
+            max_epochs=self.max_epochs,
+            gradient_clip_val=1.0,
+            callbacks=callbacks.CUDAMetricsCallback(),
+            accelerator="cuda",
+            devices="auto",
+            precision=self.precision,
+            enable_progress_bar=False,
+            enable_model_summary=False,
+            enable_checkpointing=False,
+            logger=False,
+            replace_sampler_ddp=False,
+        )
+
+        trainer.fit(model, dataloader)
+
+    def run(self):
+        model, dataloader = self.create()
+
+        self.run_benchmark(
+            self.train,
+            args=(model, dataloader),
+            num_runs=10
+        )
+
+        model, dataloader = self.create()
+        model = torch.compile(model)
+
+        self.run_benchmark(
+            self.train,
+            args=(model, dataloader),
+            num_runs=10
+        )
+
+
+app = L.LightningApp(
+    GPTBench(cloud_compute=L.CloudCompute("gpu-fast")),
+)
---
+++ lightning_mingpt/bench.py
@@ -0,0 +1,38 @@
+import gc
+import time
+
+import torch
+
+import lightning as L
+
+
+class Bench(L.LightningWork):
+    def __init__(self, num_runs=10):
+        self.num_runs = num_runs
+
+    def run_benchmark(self, fn, args=[], kwargs={}, device_type="auto"):
+        """Returns an array with the last loss from each epoch for each run."""
+        hist_losses = []
+        hist_durations = []
+        hist_memory = []
+    
+        if device_type == "auto":
+            device_type = "cuda" if torch.cuda.is_available() else "cpu"
+        torch.backends.cudnn.deterministic = True
+        for i in range(self.num_runs):
+            gc.collect()
+            if device_type == "cuda":
+                torch.cuda.empty_cache()
+                torch.cuda.reset_accumulated_memory_stats()
+                torch.cuda.reset_peak_memory_stats()
+            time.sleep(1)
+    
+            time_start = time.perf_counter()
+            final_loss, used_memory = fn(*args, **kwargs)
+            time_end = time.perf_counter()
+    
+            hist_losses.append(final_loss)
+            hist_durations.append(time_end - time_start)
+            hist_memory.append(used_memory)
+    
+        return {"losses": hist_losses, "durations": hist_durations, "memory": hist_memory}
