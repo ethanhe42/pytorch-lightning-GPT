d3419b3b54f83c691b4e8bd90c432745046f34d6
Justus Schock <justus.schock@posteo.de>
2023-01-05 15:59:37 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,8 @@ setup(
     author_email="",
     url="",
     # nanogpt is not yet configured as a package
-    packages=find_packages(exclude=["tests", "docs"]) + find_packages('mingpt', exclude=['projects', 'tests']) + ['nanogpt'],
+    packages=find_packages(exclude=["tests", "docs"]) + find_packages(where='./mingpt', exclude=['projects', 'tests']) + ['nanogpt'],
+    package_dir={'mingpt': './mingpt/mingpt'},
     long_description="",
     long_description_content_type="text/markdown",
     include_package_data=True,
